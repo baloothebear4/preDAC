fba25cc7965f432354d1485c63746d54eef8ab1e
Steve Charles <steve@silver.home>
2020-04-29 16:07:06 +0100

    Checkout new dev environment

--- preDACtest.py
+++ preDACtest.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
+"""
+    preDAC Test harness
 
+    Main test harness for testing the preDAC HW
+    uses the core classes and wraps in a user interface
+    for testing the HW board
+
+    baloothebear4 April 2020
+
+"""
+    
 from smbus2 import SMBus
 import time, sys
 import struct, math
