ca23cefa43d2314f33e726eccbede17de071fa94
Steve Charles <steve@silver.home>
2020-05-15 15:38:32 +0100

    refactoring of preDAC2

---
+++ screens.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+ preDAC preamplifier project
+
+    - top level module of Screens used in the preDAC
+    - drives the execution of the current screen
+    - is controlled by the Controller events
+    - uses data from the Platform Data Model
+
+ Baloothebear4
+ v2 May  20 - refactored from display.py
+
+"""
